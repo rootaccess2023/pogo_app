0f020cc2079a305c6c2cd4d9d88ea6507a85c418
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-19 22:40:26 +0000

    chore: export and import of MapBox container

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,10 +1,12 @@
+import { MapBox } from "./layout";
+
 export function AppContainer() {
   return (
     <div className="h-screen flex">
       <div className="w-96 bg-red-200">Side BAR</div>
-      <div className="w-full bg-red-400">
+      <div className="w-full">
         <div className="h-[50px] bg-red-300">NAV</div>
-        <div>MAP</div>
+        <MapBox />
       </div>
     </div>
   );
