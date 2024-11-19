e9007b5ff4bb38ce18a7b3c154ac64a8b690b99d
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-19 23:39:07 +0000

    chore: import Title component and add some styles

---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { Title } from "../components";
+
+export function Sidebar() {
+  return (
+    <div className="w-96">
+      <div className="p-2">
+        <Title />
+      </div>
+    </div>
+  );
+}
