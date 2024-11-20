ce0d3e2c7620cbcd34a1d2eeab4705d5aeaeaa56
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-20 00:35:08 +0000

    styles(sidebar): change to text-xs the Subtitle component

--- src/components/Sidebar/Subtitle.tsx
+++ src/components/Sidebar/Subtitle.tsx
@@ -1,5 +1,5 @@
 export function Subtitle() {
   return (
-    <p className="text-sm font-bold tracking-wide">LIST OF POGO LOCATIONS</p>
+    <p className="text-xs font-bold tracking-wide">LIST OF POGO LOCATIONS</p>
   );
 }
