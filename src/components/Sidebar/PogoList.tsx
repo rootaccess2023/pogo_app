fee37ef99fc11b2a5a71ea84d73e0cc3658282e0
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-20 00:28:19 +0000

    styles(sidebar): add styles to PogoList component

--- src/components/Sidebar/PogoList.tsx
+++ src/components/Sidebar/PogoList.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import mockPOGOLocations from "../../mock";
 import { usePogoStore, useYearStore } from "../../stores";
+import { PiPokerChipLight } from "react-icons/pi";
+import { IoIosArrowForward } from "react-icons/io";
 
 export function PogoList() {
   const year = useYearStore((state) => state.year);
@@ -16,8 +18,15 @@ export function PogoList() {
   return (
     <ul>
       {pogo.map((pogo, index) => (
-        <li key={index}>
-          <h1>{pogo.name}</h1>
+        <li
+          className="h-[60px] flex justify-between items-center hover:bg-gray-100 pl-8 pr-4 cursor-pointer"
+          key={index}
+        >
+          <span className="flex items-center gap-1">
+            <PiPokerChipLight className="size-6 text-[#007ce1]" />
+            <h1>{pogo.name}</h1>
+          </span>
+          <IoIosArrowForward className="size-6 text-[#cccccc]" />
         </li>
       ))}
       {pogo.length === 0 && <li>No data found for the selected year.</li>}
