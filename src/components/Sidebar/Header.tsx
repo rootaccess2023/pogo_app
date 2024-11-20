3d441538efb8c01784e3e7cf73fd539cd023e35a
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-20 10:01:58 +0000

    feat(sidebar): create Header component

---
+++ src/components/Sidebar/Header.tsx
@@ -0,0 +1,9 @@
+import pagcorLogo from "../../assets/pagcor_logo.svg";
+
+export function Header() {
+  return (
+    <header className="mb-3">
+      <img className="h-12" src={pagcorLogo} alt="" />
+    </header>
+  );
+}
