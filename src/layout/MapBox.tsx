ce647add564dee6a037bed5f1c2f1680f0a42287
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-20 00:28:49 +0000

    feat(map): add Markers for the pogo to the MapBox component

--- src/layout/MapBox.tsx
+++ src/layout/MapBox.tsx
@@ -1,7 +1,9 @@
 import { Map, Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { usePogoStore } from "../stores";
 
 export function MapBox() {
+  const pogo = usePogoStore((state) => state.pogo);
   const MAPBOX_TOKEN =
     "pk.eyJ1IjoicG9sb3JldmlsbzE5IiwiYSI6ImNtM2xydzVyZTByZ2Qyc3BlZ2gwaGcwZnEifQ.pQZoqD9Md_xq2JAlYZzoLw";
   return (
@@ -16,11 +18,13 @@ export function MapBox() {
         style={{ width: "100%", height: "100vh" }}
         mapStyle="mapbox://styles/mapbox/streets-v9"
       >
-        <Marker
-          longitude={121.7744}
-          latitude={12.8797}
-          anchor="bottom"
-        ></Marker>
+        {pogo.map((pogo) => (
+          <Marker
+            key={pogo.id}
+            longitude={pogo.longitude}
+            latitude={pogo.latitude}
+          ></Marker>
+        ))}
       </Map>
     </div>
   );
