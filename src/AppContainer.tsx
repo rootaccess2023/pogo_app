e2106000a9fee3df205b338177f0186241bd4c57
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-20 23:38:42 +0000

    chore: export and import of Details component

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -1,9 +1,10 @@
-import { MapBox, Sidebar, View } from "./layout";
+import { Details, MapBox, Sidebar, View } from "./layout";
 
 export function AppContainer() {
   return (
     <div className="h-screen flex">
       <Sidebar />
+      <Details />
       <div className="w-full">
         <View />
         <MapBox />
