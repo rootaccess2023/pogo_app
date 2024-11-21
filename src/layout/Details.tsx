42df1b97ced1b2d2676da0f0142bd56780bb7c9e
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-21 12:48:40 +0000

    feat(map): create Details component

--- src/layout/Details.tsx
+++ src/layout/Details.tsx
@@ -1,3 +1,7 @@
 export function Details() {
-  return <div className="w-96 bg-[#f5fbfd]"></div>;
+  return (
+    <div className="absolute top-[4.5rem] left-[1.5rem] w-[20rem] h-[40rem] bg-white bg-opacity-80 rounded-lg">
+      <p className="text-center text-black">Content inside the div</p>
+    </div>
+  );
 }
