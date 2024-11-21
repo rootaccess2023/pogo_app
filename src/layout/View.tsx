894eb62efceb4331ba1f96df3f65ca6ac6b296e1
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-21 15:15:48 +0000

    feat(map): change view width to calc(100% - 304px) to fit the screen width

--- src/layout/View.tsx
+++ src/layout/View.tsx
@@ -1,6 +1,6 @@
 export function View() {
   return (
-    <div className="absolute z-50 h-[50px] w-full flex pl-[30px] font-montserrat font-bold text-sm bg-white border-b-[3px] border-[#eeeeee]">
+    <div className="absolute z-50 h-[50px] w-[calc(100%_-_304px)] flex pl-[30px] font-montserrat font-bold text-sm bg-white border-b-[3px] border-[#eeeeee]">
       <div className="relative top-[3px] pt-[14px] px-8 border-b-[3px] border-[#f05449] hover:border-[#eeeeee] cursor-pointer">
         Map View
       </div>
