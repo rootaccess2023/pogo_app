c024f213de4f085c2625399c8d019a19eb2c7b3e
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-22 12:57:59 +0000

    refactor(map): change to backend live link

--- src/components/Sidebar/PogoList.tsx
+++ src/components/Sidebar/PogoList.tsx
@@ -19,7 +19,7 @@ export function PogoList() {
     const fetchLocations = async () => {
       try {
         const response = await fetch(
-          `https://pogoph-api.onrender.com/pogolocations?year=${year}`
+          `https://pogoph-api.onrender.com/locations?year=${year}`
         );
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
