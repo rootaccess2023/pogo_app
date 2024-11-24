e5b3334a1d1e0b365f371a98ebceb54c8d7cd036
Paulo Oliver <134943577+rootaccess2023@users.noreply.github.com>
2024-11-24 22:34:04 +0100

    styles(sidebar): add scroll into view function

--- src/components/Sidebar/PogoList.tsx
+++ src/components/Sidebar/PogoList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { usePogoStore, useYearStore } from "../../stores";
 import { PiPokerChipLight } from "react-icons/pi";
 import { IoIosArrowForward } from "react-icons/io";
@@ -8,18 +8,29 @@ export function PogoList() {
   const year = useYearStore((state) => state.year);
   const pogo = usePogoStore((state) => state.pogo);
   const setPogo = usePogoStore((state) => state.setPogo);
+  const selectedLocation = usePogoStore((state) => state.selectedLocation);
   const setSelectedLocation = usePogoStore(
     (state) => state.setSelectedLocation
   );
   const setToggleTrue = usePogoStore((state) => state.setToggleTrue);
   const setPitch = useViewStore((state) => state.setPitch);
   const setZoom = useViewStore((state) => state.setZoom);
+  const scrollPosition = useViewStore((state) => state.scrollPosition);
+  const setScrollPosition = useViewStore((state) => state.setScrollPosition);
+  const setItemRefs = useViewStore((state) => state.setItemRefs);
+  const itemRef = useViewStore((state) => state.itemRefs);
+
+  const itemRefs = useRef<(HTMLLIElement | null)[]>(itemRef);
+
+  useEffect(() => {
+    setItemRefs(itemRefs.current);
+  }, [pogo]);
 
   useEffect(() => {
     const fetchLocations = async () => {
       try {
         const response = await fetch(
-          `https://pogoph-api.onrender.com/locations?year=${year}`
+          `http://localhost:3000/locations?year=${year}`
         );
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
@@ -35,28 +46,51 @@ export function PogoList() {
     fetchLocations();
   }, [year]);
 
-  console.log(pogo);
+  const handleClick = (index: number, pogo: any) => {
+    setSelectedLocation({
+      name: pogo.name,
+      latitude: pogo.latitude,
+      longitude: pogo.longitude,
+      description: pogo.description,
+      image: pogo.image,
+      address: pogo.address,
+      years: pogo.years,
+    });
+    setToggleTrue();
+    setPitch(60);
+    setZoom(18);
+
+    const scrollPos = itemRefs.current[index]?.offsetTop || 0;
+    setScrollPosition(scrollPos);
+
+    itemRefs.current[index]?.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+  };
+
+  useEffect(() => {
+    if (scrollPosition) {
+      window.scrollTo({
+        top: scrollPosition,
+        behavior: "smooth",
+      });
+    }
+  }, [scrollPosition]);
 
   return (
     <ul className="h-[calc(100vh_-_203px)] overflow-scroll">
       {pogo.map((pogo, index) => (
         <li
-          className="h-[60px] flex justify-between items-center hover:bg-gray-100 pl-8 pr-4 cursor-pointer"
+          className={`h-[60px] flex justify-between items-center pl-8 pr-4 cursor-pointer ${
+            pogo.latitude === selectedLocation?.latitude &&
+            pogo.longitude === selectedLocation?.longitude
+              ? "font-semibold bg-gray-100"
+              : ""
+          } hover:bg-gray-100`}
           key={index}
-          onClick={() => {
-            setSelectedLocation({
-              name: pogo.name,
-              latitude: pogo.latitude,
-              longitude: pogo.longitude,
-              description: pogo.description,
-              image: pogo.image,
-              address: pogo.address,
-              years: pogo.years,
-            });
-            setToggleTrue();
-            setPitch(60);
-            setZoom(18);
-          }}
+          ref={(el) => (itemRefs.current[index] = el)}
+          onClick={() => handleClick(index, pogo)}
         >
           <span className="flex gap-1">
             <PiPokerChipLight className="size-6 text-[#007ce1]" />
--- src/stores/viewStore.ts
+++ src/stores/viewStore.ts
@@ -5,6 +5,10 @@ interface ViewState {
   setZoom: (zoom: number) => void;
   pitch: number;
   setPitch: (pitch: number) => void;
+  scrollPosition: number;
+  setScrollPosition: (position: number) => void;
+  itemRefs: (HTMLLIElement | null)[];
+  setItemRefs: (refs: (HTMLLIElement | null)[]) => void;
 }
 
 export const useViewStore = create<ViewState>((set) => ({
@@ -12,4 +16,8 @@ export const useViewStore = create<ViewState>((set) => ({
   setZoom: (zoom: number) => set({ zoom }),
   pitch: 0,
   setPitch: (pitch: number) => set({ pitch }),
+  scrollPosition: 0, // Initialize scroll position
+  setScrollPosition: (position: number) => set({ scrollPosition: position }),
+  itemRefs: [],
+  setItemRefs: (refs: (HTMLLIElement | null)[]) => set({ itemRefs: refs }),
 }));
